1cb055d93f5235f10b821333996daa612252c8b1
Andre Bonfatti <andrebnf@gmail.com>
2015-11-17 23:38:50 -0200

    fixed kmeans output

--- kmeans.rb
+++ kmeans.rb
@@ -8,8 +8,8 @@ unless ARGV.size == 2
   abort
 end
 
-# points = FileReader.read ARGV[0]
-points    = FileReader.read "data/c2ds1-2sp.txt"
+# points, names = FileReader.read "data/c2ds1-2sp.txt"
+points, names = FileReader.read "data/" + ARGV[0]
 clusters  = []
 n         = points.size
 k         = ARGV[1].to_i
@@ -34,3 +34,10 @@ points.each do |p|
 end
 
 Plotter.plot x, y, c
+
+out = ""
+for i in 0..points.size-1
+  out += names[i] + "\t" + points[i].cluster_id.to_s + "\n"
+end
+
+File.open("./data_out_kmeans/c2ds1-2sp.txt.out", 'w') { |file| file.write(out) }
--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -4,13 +4,16 @@ class FileReader
   def self.read file_name
     file = File.new(file_name, "r")
     points = []
+    names = []
     line = file.gets # header
     while line = file.gets
+      n = line.split("\t")[0]
       x = line.split("\t")[1]
       y = line.split("\t")[2].split("\n")[0]
       point = Point.new x.to_f, y.to_f
       points << point
+      names  << n
     end
-    return points
+    return points, names
   end
 end
